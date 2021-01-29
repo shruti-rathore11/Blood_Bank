4462544e5ea10d78041a3c196cc2a9efd82e12dc
Shruti Rathore <72699771+shruti-rathore11@users.noreply.github.com>
2021-01-30 03:56:48 +0530

    Add files via upload

---
+++ using/Codes/view.php
@@ -0,0 +1,9 @@
+<div class="result-each">
+	
+	<div class="r1"><h3><i class="fa fa-user"></i> Deepika</h3></div> 
+	<div class="r3">A+</div> <!-- <div class="r6"><i class="fa fa-map-marker"></i> cxvcvcvb</div> --> 
+	<div class="r5"><i class="fa fa-map-marker"></i> Agartala</div> 
+	<div class="r5-1"><i class="fa fa-calendar"></i> Last blood donated date: Yet to donate</div> 
+	<div class="r2"> <a href="https://www.bloodbankindia.net/donors/email_request?id=1038" class="r2-btn"><i class="fa fa-envelope"></i> Send Mail</a></div> 
+	<div class="r4"> <a href="https://www.bloodbankindia.net/donors/request?id=1038" class="r4-btn"><i class="fa fa-eye" aria-hidden="true"></i> View Contact Details </a></div> 
+</div>
